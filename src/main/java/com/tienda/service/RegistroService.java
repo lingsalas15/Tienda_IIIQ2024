0fea7621a92a5b673ce4e58e34a662d4bcd69c38
dsala <dsala@Darling>
2024-11-26 20:55:24 -0600

    Semana 11

---
+++ src/main/java/com/tienda/service/RegistroService.java
@@ -0,0 +1,20 @@
+package com.tienda.service;
+
+import com.tienda.domain.Usuario;
+import jakarta.mail.MessagingException;
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface RegistroService {
+    
+    //se usa el objeto Model
+
+    public Model activar(Model model, String usuario, String clave);
+
+    public Model crearUsuario(Model model, Usuario usuario) throws MessagingException;
+    
+    public void activar(Usuario usuario, MultipartFile imagenFile);
+    
+    public Model recordarUsuario(Model model, Usuario usuario) throws MessagingException;
+}
+
